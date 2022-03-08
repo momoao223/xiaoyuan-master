5f5861657df0af32d98c17e306d75d08887e1189
nansan <3303206609@qq.com>
2022-03-08 17:33:55 +0800

    first commit

---
+++ server/plugins/DataBase.ini.js
@@ -0,0 +1,8 @@
+const Database = {}
+
+Database.host = '127.0.0.1'//ip
+Database.user = 'root'//用户名
+Database.password = '123123'//密码
+Database.port = 3306//端口
+Database.database = 'changhangtong'//数据库名
+exports.dbmysql =  Database
